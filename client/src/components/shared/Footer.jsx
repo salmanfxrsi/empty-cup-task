20c9c10fc366c87edc4d2a4e49c5ee6932559fc6
salmanfxrsi <salmanxprivate@gmail.com>
2025-02-23 09:10:55 +0600

    feat(client): add footer as a shared component

---
+++ client/src/components/shared/Footer.jsx
@@ -0,0 +1,58 @@
+const Footer = () => {
+  return (
+    <div className="bg-black text-white">
+      {/* Footer Navigation Links */}
+      <footer className="container mx-auto p-10 footer sm:footer-horizontal">
+        <aside className="flex flex-col items-start">
+          <img
+            className="w-16 h-16"
+            src="https://i.ibb.co.com/kV2MDDKC/small-logo.png"
+            alt="Empty Cup Logo"
+          />
+          <p>
+            Empty Cup
+            <br />
+            1000+ houses designed since January 2020
+          </p>
+        </aside>
+
+        {/** Navigation Sections */}
+        {[
+          {
+            title: "Services",
+            links: ["Branding", "Design", "Marketing", "Advertisement"],
+          },
+          {
+            title: "Company",
+            links: ["About us", "Contact", "Jobs", "Press kit"],
+          },
+          {
+            title: "Legal",
+            links: ["Terms of use", "Privacy policy", "Cookie policy"],
+          },
+        ].map((section, index) => (
+          <nav key={index}>
+            <h6 className="footer-title">{section.title}</h6>
+            {section.links.map((link, i) => (
+              <a key={i} className="link link-hover">
+                {link}
+              </a>
+            ))}
+          </nav>
+        ))}
+      </footer>
+
+      {/* Footer Copyright Section */}
+      <footer className="footer footer-center p-4">
+        <aside>
+          <p>
+            Copyright &copy; {new Date().getFullYear()} - All rights reserved by
+            Empty Cup Ltd
+          </p>
+        </aside>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/shared/Navbar";
+import Footer from "../components/shared/Footer";
 
 const MainLayout = () => {
   return (
@@ -10,12 +11,14 @@ const MainLayout = () => {
       </nav>
 
       {/* Main Content */}
-      <main>
+      <main className="h-screen">
         <Outlet />
       </main>
 
       {/* Footer */}
-      <footer></footer>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 };
